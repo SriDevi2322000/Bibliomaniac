ab761438e36203941572008c286a408508ad8a33
SriDevi2322000 <54175377+SriDevi2322000@users.noreply.github.com>
2020-06-06 15:54:08 +0530

    Add files via upload

--- categories.php
+++ categories.php
@@ -34,7 +34,7 @@ $catt_result = mysqli_query($conn, $catt_sql);
 <!DOCTYPE html>
 <html lang="en">
 <head>
-<title>Colo Shop Categories</title>
+<title>Book Categories</title>
 <meta charset="utf-8">
 <meta http-equiv="X-UA-Compatible" content="IE=edge">
 <meta name="description" content="Colo Shop Template">
@@ -88,7 +88,7 @@ $catt_result = mysqli_query($conn, $catt_sql);
 						</ul>
 					</div>
 
-					<!-- Price Range Filtering -->
+					<!-- Price Range Filtering -->ss
 					<div class="sidebar_section">
 						<div class="sidebar_title">
 							<h5>Filter by Price</h5>
@@ -173,12 +173,16 @@ $catt_result = mysqli_query($conn, $catt_sql);
                                         </span></div>
                                         <div class="product_info">
                                             <h6 class="product_name"><a href="bookview.php?id=<?php echo $row[0]; ?>"><?php echo $row[2]; ?></a></h6>
-                                            <div class="product_price"><?php if($row[6] == 1){ echo '$'.$row[7]; }elseif ($row[6] == 2) {
+                                            <div class="product_price"><?php if($row[6] == 1){ echo 'INR '.$row[7]; }elseif ($row[6] == 2) {
                                             echo "No of days : ",$row[11];
                                         }else{ echo "Free"; } ?></div>
                                         </div>
-                                        </div>
-                                        <div class="red_button add_to_cart_button"><a href="#">add to cart</a></div>
+										</div>
+										<?php if($row[9] != 0){ ?>
+											<div class="red_button add_to_cart_button"><a href="bookview.php?id=<?php echo $row[0]; ?>">Buy Now</a></div>
+										<?php }else{ ?>
+											<div class="red_button add_to_cart_button"><a href="#">Out of stock</a></div>
+										<?php } ?>
                                     </div>
 
                                 <?php }while ($row = $result -> fetch_row()); }else{ echo "No books found"; } ?>
@@ -196,108 +200,14 @@ $catt_result = mysqli_query($conn, $catt_sql);
 		</div>
 	</div>
 
-	<!-- Benefit -->
-
-	<div class="benefit">
-		<div class="container">
-			<div class="row benefit_row">
-				<div class="col-lg-3 benefit_col">
-					<div class="benefit_item d-flex flex-row align-items-center">
-						<div class="benefit_icon"><i class="fa fa-truck" aria-hidden="true"></i></div>
-						<div class="benefit_content">
-							<h6>free shipping</h6>
-							<p>Suffered Alteration in Some Form</p>
-						</div>
-					</div>
-				</div>
-				<div class="col-lg-3 benefit_col">
-					<div class="benefit_item d-flex flex-row align-items-center">
-						<div class="benefit_icon"><i class="fa fa-money" aria-hidden="true"></i></div>
-						<div class="benefit_content">
-							<h6>cach on delivery</h6>
-							<p>The Internet Tend To Repeat</p>
-						</div>
-					</div>
-				</div>
-				<div class="col-lg-3 benefit_col">
-					<div class="benefit_item d-flex flex-row align-items-center">
-						<div class="benefit_icon"><i class="fa fa-undo" aria-hidden="true"></i></div>
-						<div class="benefit_content">
-							<h6>45 days return</h6>
-							<p>Making it Look Like Readable</p>
-						</div>
-					</div>
-				</div>
-				<div class="col-lg-3 benefit_col">
-					<div class="benefit_item d-flex flex-row align-items-center">
-						<div class="benefit_icon"><i class="fa fa-clock-o" aria-hidden="true"></i></div>
-						<div class="benefit_content">
-							<h6>opening all week</h6>
-							<p>8AM - 09PM</p>
-						</div>
-					</div>
-				</div>
-			</div>
-		</div>
-	</div>
-
+	
 	<!-- Newsletter -->
 
-	<div class="newsletter">
-		<div class="container">
-			<div class="row">
-				<div class="col-lg-6">
-					<div class="newsletter_text d-flex flex-column justify-content-center align-items-lg-start align-items-md-center text-center">
-						<h4>Newsletter</h4>
-						<p>Subscribe to our newsletter and get 20% off your first purchase</p>
-					</div>
-				</div>
-				<div class="col-lg-6">
-					<div class="newsletter_form d-flex flex-md-row flex-column flex-xs-column align-items-center justify-content-lg-end justify-content-center">
-						<input id="newsletter_email" type="email" placeholder="Your email" required="required" data-error="Valid email is required.">
-						<button id="newsletter_submit" type="submit" class="newsletter_submit_btn trans_300" value="Submit">subscribe</button>
-					</div>
-				</div>
-			</div>
-		</div>
-	</div>
+	<?php include("news.php"); ?>
+	<?php include("Footer.php"); ?>
 
-	<!-- Footer -->
 
-	<footer class="footer">
-		<div class="container">
-			<div class="row">
-				<div class="col-lg-6">
-					<div class="footer_nav_container d-flex flex-sm-row flex-column align-items-center justify-content-lg-start justify-content-center text-center">
-						<ul class="footer_nav">
-							<li><a href="#">Blog</a></li>
-							<li><a href="#">FAQs</a></li>
-							<li><a href="contact.html">Contact us</a></li>
-						</ul>
-					</div>
-				</div>
-				<div class="col-lg-6">
-					<div class="footer_social d-flex flex-row align-items-center justify-content-lg-end justify-content-center">
-						<ul>
-							<li><a href="#"><i class="fa fa-facebook" aria-hidden="true"></i></a></li>
-							<li><a href="#"><i class="fa fa-twitter" aria-hidden="true"></i></a></li>
-							<li><a href="#"><i class="fa fa-instagram" aria-hidden="true"></i></a></li>
-							<li><a href="#"><i class="fa fa-skype" aria-hidden="true"></i></a></li>
-							<li><a href="#"><i class="fa fa-pinterest" aria-hidden="true"></i></a></li>
-						</ul>
-					</div>
-				</div>
-			</div>
-			<div class="row">
-				<div class="col-lg-12">
-					<div class="footer_nav_container">
-						<div class="cr"> Template made with <i class="fa fa-heart-o" aria-hidden="true"></i> <a href="#">By girls</a></div>
-					</div>
-				</div>
-			</div>
-		</div>
-	</footer>
-
+	
 </div>
 
 <script src="js/jquery-3.2.1.min.js"></script>
